70efc6dd17da04deb8b461428ad8ac592ba77dcb
Semyon Belokovsky <igrampe@gmail.com>
2015-09-06 01:52:22 +0500

    Initial Commit

---
+++ BlurNavigationController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlurNavigationController
+//
+//  Created by Semyon Belokovsky on 06/09/15.
+//  Copyright (c) 2015 App Plus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlurNavigationController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlurNavigationController
+//
+//  Created by Semyon Belokovsky on 06/09/15.
+//  Copyright (c) 2015 App Plus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
